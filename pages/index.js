9b18c250840536989c6035988e8fc5fee8c6193a
Dominic Torruellas <dom@domtorr.com>
2019-01-21 22:44:55 -0700

    finished nextjs tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,57 @@
 import Link from "next/link";
 import Layout from "../components/MyLayout";
 
-import fetch from "isomorphic-unfetch";
+function getPosts() {
+  return [
+    { id: "hello-textjs", title: "Hello Next.js" },
+    { id: "learn-nexjs", title: "Learn Next.js is awesome" },
+    { id: "deploy-nextjs", title: "Deploy apps with ZEIT" }
+  ];
+}
 
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(({ show }) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+
+    <style jsx>{`
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a: hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
 );
 
-Index.getInitialProps = async function() {
-  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
-  const data = await res.json();
-
-  console.log(`Show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data
-  };
-};
+const Index = () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      {getPosts().map(post => (
+        <PostLink key={post.id} post={post} />
+      ))}
+    </ul>
+    <style jsx global>{`
+      h1,
+      a {
+        font-family: "Arial";
+      }
+    `}</style>
+  </Layout>
+);
 
 export default Index;
